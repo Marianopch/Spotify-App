fe168f150cd05445787152ab8687b38812a8efbf
Mariano Patricio Rosi <you@example.com>
2021-12-21 22:39:12 -0300

    primer commit

---
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-media-player',
+  templateUrl: './media-player.component.html',
+  styleUrls: ['./media-player.component.css']
+})
+export class MediaPlayerComponent implements OnInit {
+
+  mockCover: any ={
+    cover: "",
+    name:"",
+    album:""
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
